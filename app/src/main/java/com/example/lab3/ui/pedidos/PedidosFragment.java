b26d0211f02c67de83248be4c99639672a5db3c5
Jose Luis Apaza Cueva <jlapazac@grupokonecta.com>
2022-03-23 20:58:28 -0500

    Configurar boton de Pedido

--- app/src/main/java/com/example/lab3/ui/pedidos/PedidosFragment.java
+++ app/src/main/java/com/example/lab3/ui/pedidos/PedidosFragment.java
@@ -2,6 +2,7 @@ package com.example.lab3.ui.pedidos;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,8 +12,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.lab3.MainActivity;
 import com.example.lab3.R;
+import com.example.lab3.databinding.FragmentHomeBinding;
+import com.example.lab3.ui.home.HomeViewModel;
 
 public class PedidosFragment extends Fragment {
 
@@ -25,14 +31,15 @@ public class PedidosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pedidos_fragment, container, false);
-    }
+        mViewModel =
+                new ViewModelProvider(this).get(PedidosViewModel.class);
+        View root = inflater.inflate(R.layout.pedidos_fragment, container, false);
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PedidosViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        Button b1 = root.findViewById(R.id.button);
+        b1.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), MainActivity.class));
+        });
 
+        return root;
+    }
 }
