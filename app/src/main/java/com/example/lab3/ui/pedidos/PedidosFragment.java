ef9ee78a406d3f6d8a3e9b99fdaee8a6edf8a029
Jose Luis Apaza Cueva <jlapazac@grupokonecta.com>
2022-03-23 20:24:34 -0500

    Se agrego el frament de pedido

---
+++ app/src/main/java/com/example/lab3/ui/pedidos/PedidosFragment.java
@@ -0,0 +1,38 @@
+package com.example.lab3.ui.pedidos;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lab3.R;
+
+public class PedidosFragment extends Fragment {
+
+    private PedidosViewModel mViewModel;
+
+    public static PedidosFragment newInstance() {
+        return new PedidosFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pedidos_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PedidosViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/lab3/ui/pedidos/PedidosViewModel.java
@@ -0,0 +1,7 @@
+package com.example.lab3.ui.pedidos;
+
+import androidx.lifecycle.ViewModel;
+
+public class PedidosViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
